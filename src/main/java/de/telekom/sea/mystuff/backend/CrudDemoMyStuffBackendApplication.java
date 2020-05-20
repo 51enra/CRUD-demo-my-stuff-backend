b3572a7f918d7381c1bca49f00ffcd56657b7ad7
Arne Chrestin <arne.chrestin@telekom.de>
2020-05-20 10:15:20 +0200

    initial commit

---
+++ src/main/java/de/telekom/sea/mystuff/backend/CrudDemoMyStuffBackendApplication.java
@@ -0,0 +1,13 @@
+package de.telekom.sea.mystuff.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDemoMyStuffBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDemoMyStuffBackendApplication.class, args);
+	}
+
+}
